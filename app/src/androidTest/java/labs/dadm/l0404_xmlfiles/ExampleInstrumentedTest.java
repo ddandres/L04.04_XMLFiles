8c283b4bbf175a121063b23baad5a971c0bd1782
ddandres <ddandres@disca.upv.es>
2019-12-17 10:41:31 +0100

    Update to AndroidX

--- app/src/androidTest/java/labs/dadm/l0404_xmlfiles/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/labs/dadm/l0404_xmlfiles/ExampleInstrumentedTest.java
@@ -5,8 +5,9 @@
 package labs.dadm.l0404_xmlfiles;
 
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,7 +24,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("labs.dadm.l0404_xmlfiles", appContext.getPackageName());
     }
